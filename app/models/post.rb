8141142c2bdaa31bbbe95ff187afbdf87216f3b3
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-14 17:55:24 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,20 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates(:content, { :length => {:minimum => 250}})
-  validates(:summary, { :length => {:maximum => 250}})
-  validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
-  validate :is_clickbait?
+validates :content, length: { minimum: 250 }
+validates :summary, length: { maximum: 250 }
+validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+validate :is_clickbait?
 
-  CLICKBAIT_PATTERNS = [
-    /Won't Believe/i,
-    /Secret/i,
-    /Top [0-9]*/i,
-    /Guess/i
-  ]
+CLICKBAIT_PATTERNS = [
+  /Won't Believe/i,
+  /Secret/i,
+  /Top [0-9]*/i,
+  /Guess/i
+]
 
-  def is_clickbait?
-    if CLICKBAIT_PATTERNS.none? {|pat| pat.match title}
-      errors.add(:title, "must be clickbait")
-    end
+def is_clickbait?
+  if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+    errors.add(:title, "must be clickbait")
   end
+end
 end
