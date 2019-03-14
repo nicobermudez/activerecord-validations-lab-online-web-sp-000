79934c026b6917164a75e05c219faf5e7428c524
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-14 17:50:58 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates(:content, { :length => {:minimum => 250}})
   validates(:sumamry, { :length => {:maximum => 250}})
+  
 end
