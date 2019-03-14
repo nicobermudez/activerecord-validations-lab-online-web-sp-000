d35d34245b5aa9054bc7ea1d9e900e5ea0129e49
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-14 17:54:03 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ActiveRecord::Base
   validates(:summary, { :length => {:maximum => 250}})
   validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
   validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
 end
